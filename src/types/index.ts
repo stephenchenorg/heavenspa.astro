fb87877072ee8418ad41561046bba5e6719c7650
carol <kiol870101@gmail.com>
2025-09-12 15:42:20 +0800

    no message

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './alert'
 export * from './api'
 export * from './companySetting'
+export * from './team'
 // Index file to export all types from the types directory
 export * from './user'
---
+++ src/types/team.ts
@@ -0,0 +1,49 @@
+// Team 相關的 API 回應類型定義
+
+export interface TeamTag {
+  id: number
+  title: string
+  slug: string
+}
+
+export interface TeamCover {
+  desktop: string
+  desktop_blur: string
+  mobile: string
+  mobile_blur: string
+}
+
+export interface TeamImage {
+  desktop: string
+  desktop_blur: string
+  mobile: string
+  mobile_blur: string
+}
+
+export interface TeamImageItem {
+  created_at: string
+  id: number
+  image: TeamImage
+  updated_at: string
+  is_default: boolean
+}
+
+export interface TeamCategory {
+  title: string
+}
+
+export interface TeamMember {
+  id: number
+  title: string
+  category: TeamCategory
+  cover: TeamCover
+  images: TeamImageItem[]
+  image: TeamImage
+  tags: { data: TeamTag[] }
+}
+
+export interface TeamsResponse {
+  teams: {
+    data: TeamMember[]
+  }
+}
