0bb27d7e35c89ef5079e988527554acf07ee502e
tasb00429 <tasb00429@gmail.com>
2025-09-15 10:59:50 +0800

    Fast update using script

--- src/components/ui/Prose.vue
+++ src/components/ui/Prose.vue
@@ -1,16 +1,28 @@
 <template>
-  <div
-    :class="cn('prose', props.class)"
-    v-html="content"
-  />
+  <div id="prose" ref="element" class="prose h-[2000px] w-[1500px]" />
 </template>
 
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/utils/className'
+import { computed, onMounted, ref } from 'vue'
 
-const props = defineProps<{
-  content?: string
-  class?: HTMLAttributes['class']
-}>()
+const props = defineProps({
+  sourceDoc: {
+    type: String,
+    default: null,
+  },
+})
+console.log('22223123123')
+const element = ref(null)
+console.log(element.value, '2222')
+onMounted(() => {
+  console.log(element.value, '2222')
+  if (!element.value) return
+
+  const shadowHost = computed(
+    () =>
+      element.value!.shadowRoot || element.value!.attachShadow({ mode: 'open' })
+  )
+
+  shadowHost.value.innerHTML = props.sourceDoc
+})
 </script>
