580d75c4611836db735da9dd09fef5559d2ddf70
carol <kiol870101@gmail.com>
2025-09-15 22:45:14 +0800

    no message

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,12 +1,10 @@
 import { sequence } from 'astro:middleware'
 import { auth } from './auth'
 import { guest } from './guest'
-import { locale } from './locale'
 import { originCheck } from './originCheck'
 
 export const onRequest = sequence(
   originCheck,
-  locale,  // 在 guest 和 auth 之前處理語言
   guest,
   auth,
 )
