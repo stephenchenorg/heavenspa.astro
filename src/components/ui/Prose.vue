6caaf3a5bc9ca4cb6c1ddb3e708826f6cdf35b4a
tasb00429 <tasb00429@gmail.com>
2025-09-15 14:03:20 +0800

    feat:

--- src/components/ui/Prose.vue
+++ src/components/ui/Prose.vue
@@ -1,9 +1,9 @@
 <template>
-  <div ref="element"/>
+  <div ref="element" />
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, ref} from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 const props = defineProps({
   sourceDoc: {
@@ -11,21 +11,17 @@ const props = defineProps({
     default: null,
   },
 })
-const element = ref(null)
+
+const element = ref<HTMLElement | null>(null)
 
 onMounted(() => {
-  if (!element.value) return
+  if (!element.value || !props.sourceDoc) return
 
-  const element = ref<HTMLElement | null>(null)
+  const shadowHost = computed(
+    () =>
+      element.value!.shadowRoot || element.value!.attachShadow({ mode: 'open' })
+  )
 
-  onMounted(() => {
-    if (!element.value || !props.sourceDoc) return
-
-    const shadowHost = computed(
-      () =>
-        element.value!.shadowRoot || element.value!.attachShadow({mode: 'open'})
-    )
-
-    shadowHost.value.innerHTML = props.sourceDoc
-  })
+  shadowHost.value.innerHTML = props.sourceDoc
+})
 </script>
