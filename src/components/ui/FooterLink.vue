37dc858d93e99dec176a10253875c33d88d47538
carol <kiol870101@gmail.com>
2025-09-16 14:27:34 +0800

    no message

--- src/components/ui/FooterLink.vue
+++ src/components/ui/FooterLink.vue
@@ -6,7 +6,7 @@
       style="color: var(--theme-nav-text, #000000);"
     >
       <span class="opacity-0 group-hover:opacity-100 w-0 group-hover:w-auto group-hover:mr-2 transition-all duration-300 transform -translate-x-3 group-hover:translate-x-0 overflow-hidden text-[var(--color-primary-600,#c4a428)]">→</span>
-      <span>{{ text }}</span>
+      <span class="text-sm text-[var(--theme-text-secondary,#666666)]">{{ text }}</span>
     </a>
   </li>
 </template>
