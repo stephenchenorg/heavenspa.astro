28818e3f69056ba189092fa7c059ee1ea3b55064
carol <kiol870101@gmail.com>
2025-09-16 15:10:35 +0800

    no message

---
+++ src/api/banners.ts
@@ -0,0 +1,41 @@
+import { gql, graphQLAPI } from '@/api/index'
+
+export interface BannerImage {
+  desktop: string
+  desktop_blur: string
+  mobile: string
+  mobile_blur: string
+}
+
+export interface Banner {
+  id: string
+  title: string
+  sub_title: string
+  cta_title: string
+  cta_link: string
+  image: BannerImage
+}
+
+export async function getBanners(): Promise<Banner[]> {
+  const res = await graphQLAPI(gql`
+    query MyQuery {
+      banners {
+        data {
+          cta_link
+          cta_title
+          id
+          image {
+            desktop
+            desktop_blur
+            mobile
+            mobile_blur
+          }
+          title
+          sub_title
+        }
+      }
+    }
+  `)
+
+  return res.banners?.data || []
+}
