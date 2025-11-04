4affab44774b898c55fa14cff2622ae5eec27fb6
carol <kiol870101@gmail.com>
2025-11-04 11:59:16 +0800

    no message

--- src/types/companySetting.ts
+++ src/types/companySetting.ts
@@ -1,25 +1,17 @@
-export interface CompanySettingData {
-  address_1: string
-  address_2: string
-  description: string
-  email_1: string
-  email_2: string
-  fb_link: string
-  ig_link: string
-  lang: string
-  line_link: string
-  whatsapp_link: string
-  twitter_link: string
-  threads_link: string
-  logo: string // 建議尺寸: 電腦版 240x80px (3:1)，手機版 120x40px (3:1)
+export interface Company {
+  id: number
   name: string
-  phone_1: string
-  phone_2: string
-  tg_link: string
-  vat: string
-  payment_method: string
-  business_hours: string
-}
+  description?: string
+  address?: string
+  phone?: string
+  email?: string
+  website?: string
+  business_hours?: string
+  facebook?: string
+  instagram?: string
+  youtube?: string
+  linkedin?: string
+  logo: string // 建議尺寸: 120x40px (3:1)
 
 export interface CompanySetting {
   companySetting: CompanySettingData
